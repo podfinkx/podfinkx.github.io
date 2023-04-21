c6f46f8fcdacc891256af1bff81494e13657032c
Manuel Hurtado <podfinkx@protonmail.com>
2023-04-21 12:58:18 -0400

    define about collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,12 @@
 import { defineCollection, z } from 'astro:content';
 
+const about = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+	}),
+});
+
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
