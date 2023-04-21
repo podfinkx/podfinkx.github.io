503bbbd9e23de556e73659acea25dc741ded2c66
Manuel Hurtado <podfinkx@protonmail.com>
2023-04-21 12:32:10 -0400

    Add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://podfinkx.github.io',
   integrations: [mdx(), sitemap()]
 });
